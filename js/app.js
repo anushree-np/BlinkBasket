1da1e365f62cf9c41aeee45aedc912af401bda88
Anushree <anushreenagarajan18@gmail.com>
2026-05-17 11:20:52 +0530

    Admin access simplified

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
+import { auth, db } from "./config/firebase.js";
+
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
+
 import {
-    auth,
-    db
-} from "./config/firebase.js";
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/11.7.1/firebase-firestore.js";
 
+/* =====================================================
+   AUTH CHECK
+===================================================== */
 
-console.log("Firebase Connected");
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    return;
+  }
 
-console.log(auth);
+  try {
+    const userRef = doc(db, "users", user.uid);
 
-console.log(db);
+    const snapshot = await getDoc(userRef);
+
+    if (!snapshot.exists()) {
+      return;
+    }
+
+    const userData = snapshot.data();
+
+    /* =========================
+           ADMIN BUTTON
+        ========================= */
+
+    if (userData.role === "admin") {
+      const navLinks = document.querySelector(".nav-links");
+
+      navLinks.innerHTML += `
+
+                <a href="./pages/admin.html">
+
+                    Admin
+
+                </a>
+            `;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
